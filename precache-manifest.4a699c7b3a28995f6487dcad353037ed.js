520aa765a1c9b6a2c7d59e56e5badf41917e7805
Apple <apple@Apples-MacBook-Pro.local>
2020-05-15 16:28:43 +0530

    Updates

---
+++ precache-manifest.4a699c7b3a28995f6487dcad353037ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e85166d8133fc62bfcc5d1fb9e6ef1",
+    "url": "/fakeJSON-list-react/index.html"
+  },
+  {
+    "revision": "dbbde4796bf945f3f81d",
+    "url": "/fakeJSON-list-react/static/css/main.c63e1677.chunk.css"
+  },
+  {
+    "revision": "345b91fe82375cae5889",
+    "url": "/fakeJSON-list-react/static/js/2.03d7d04b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/fakeJSON-list-react/static/js/2.03d7d04b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbbde4796bf945f3f81d",
+    "url": "/fakeJSON-list-react/static/js/main.850fe1f8.chunk.js"
+  },
+  {
+    "revision": "d7b7ccdd264691adf87c",
+    "url": "/fakeJSON-list-react/static/js/runtime-main.84a265c3.js"
+  },
+  {
+    "revision": "6ecb82f3a153e33681208af34ded6702",
+    "url": "/fakeJSON-list-react/static/media/rainbow.6ecb82f3.png"
+  }
+]);
